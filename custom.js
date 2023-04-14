3c320533c3f067510f8c6dd950b8f3ff9f551721
park <goslwjddl@naver.com>
2023-04-14 19:55:10 +0900

    feat : jquery 활용 업다운 슬라이드 구현

--- custom.js
+++ custom.js
@@ -12,8 +12,22 @@ $(function () {
   /* TypeIt - Welcome */
   $("#typing").typeIt({
     strings: ["Business strategy.", "Innovation Plan.", "Creative Idea."], // 타이핑 텍스트 입력
-    speed: 100, 
-    autoStart: true, 
-    breakLines: false, 
+    speed: 100,
+    autoStart: true,
+    breakLines: false,
+  });
+});
+// faq accordian
+
+$(function () {
+  // 첫번째 desc 는 보여지게 하기
+  $(".faq-desc").eq(0).show();
+  // 슬라이드 다운 업 기능
+  $(".faq-title").click(function () {
+    $(this).next().stop().slideDown();
+    $(this).parent().siblings().children(".faq-desc").stop().slideUp();
+    // 클릭 슬라이드 css 주기
+    $(this).parent().addClass("active");
+    $(this).parent().siblings().removeClass("active");
   });
 });
